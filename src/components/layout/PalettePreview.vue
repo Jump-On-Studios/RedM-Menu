8a0096062a5dcecc9ce16306a6fe44bfb176c16b
KadDarem <cressent.h@orange.fr>
2024-10-11 11:08:54 +0200

    auto max

--- src/components/layout/PalettePreview.vue
+++ src/components/layout/PalettePreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['colorCustom color-'+props.sliders.length]" ref="boxParent">
+  <div :class="['colorCustom color-' + props.sliders.length]" ref="boxParent">
     <div class="tint0" ref="tint0" :style="getStyleTint(0)"></div>
     <div class="tint1" ref="tint1" :style="getStyleTint(1)"></div>
     <div class="tint2" ref="tint2" :style="getStyleTint(2)"></div>
@@ -15,6 +15,7 @@ const props = defineProps(['sliders'])
 const tint0 = ref(null);
 const tint1 = ref(null);
 const tint2 = ref(null);
+const max = ref(1)
 
 let palette = ""
 for (let index = 0; index < props.sliders.length; index++) {
@@ -26,14 +27,21 @@ for (let index = 0; index < props.sliders.length; index++) {
 }
 let url = `./assets/images/menu/${palette}.png`;
 
-function  getStyleTint(index) {
+const img = new Image();
+img.src = url;
+
+img.onload = function () {
+  max.value = img.naturalWidth - 1; // Largeur originale de l'image
+};
+
+function getStyleTint(index) {
   let slider
 
   let counter = 0
   for (let i = 0; i < props.sliders.length; i++) {
     slider = props.sliders[i];
     if (slider.type == "palette") {
-      if (counter == index )
+      if (counter == index)
         break
       counter++
     }
@@ -41,10 +49,10 @@ function  getStyleTint(index) {
   if (counter < index)
     return {}
 
-  let percent = (slider.current/slider.max)*100
+  let percent = (slider.current / max.value) * 100
   return {
-    backgroundImage: "url("+url+")",
-    backgroundPosition: percent+ "% 0px"
+    backgroundImage: "url(" + url + ")",
+    backgroundPosition: percent + "% 0px"
   }
 }
 </script>
--- src/components/layout/sliders/ColorPalette.vue
+++ src/components/layout/sliders/ColorPalette.vue
@@ -7,77 +7,78 @@
       <div class="arrow right clicker" @click="menuStore.sliderRight(props.index)"><img src="/assets/images/menu/selection_arrow_right.png"></div>
     </div>
     <div class="colorSlider">
-      <div :class="['keyHelpers','index-'+props.index]" v-if="menuStore.cItem.sliders.length > 1">
-        <div :class="['left',{'qwerty':menuStore.isQwerty && props.index==1}]" ref="keyLeft">
+      <div :class="['keyHelpers', 'index-' + props.index]" v-if="menuStore.cItem.sliders.length > 1">
+        <div :class="['left', { 'qwerty': menuStore.isQwerty && props.index == 1 }]" ref="keyLeft">
           {{ leftKey() }}
         </div>
         <div class="right" ref="keyRight">
           {{ rightKey() }}
         </div>
       </div>
-      <input
-        type="range"
-        min=0
-        :max="props.slider.max"
-        :class="['palette','max-'+props.slider.max]"
-        :style="background()"
-        :value="props.slider.current" 
-        @input="change"
-      />
+      <input type="range" min=0 :max="max" :class="['palette', 'max-' + max]" :style="background()" :value="props.slider.current" @input="change" />
     </div>
   </div>
 </template>
 
 <script setup>
-  import { onBeforeUnmount, onMounted, inject} from 'vue';
-  import { useLangStore } from '../../../stores/lang';
-  const lang = useLangStore().lang
-  import { useMenuStore } from '../../../stores/menus';
-  const menuStore = useMenuStore()
-  const API = inject('API')
+import { onBeforeUnmount, onMounted, inject, ref } from 'vue';
+import { useLangStore } from '../../../stores/lang';
+const lang = useLangStore().lang
+import { useMenuStore } from '../../../stores/menus';
+const menuStore = useMenuStore()
+const API = inject('API')
 
-  const props = defineProps(['index','slider'])
+const props = defineProps(['index', 'slider'])
+const max = ref(1)
+let mounted = false
 
-  let mounted = false
-  
-  function background() {
-    return {backgroundImage:`url(./assets/images/menu/${props.slider.tint}.png`}
-  }
-  function numItem() {
-    return API.sprintf(lang('of'),props.slider.current+1, props.slider.max+1)
-  }
-  function getTitle() {
-    if (!props.slider.translate) return props.slider.title
-    return lang(props.slider.title)
-  }
-  function change(e) {
-    if (!mounted) return
-    
-    let value = e.target.value
-    e.target.blur()
+let url = `./assets/images/menu/${props.slider.tint}.png`;
 
-    if (value == props.slider.current) return
-    menuStore.setSliderCurrent({index: props.index,value:parseInt(value)})
+const img = new Image();
+img.src = url;
+
+img.onload = function () {
+  max.value = img.naturalWidth - 1; // Largeur originale de l'image
+};
+
+function background() {
+  return { backgroundImage: `url(${url}` }
+}
+function numItem() {
+  return API.sprintf(lang('of'), props.slider.current + 1, max.value + 1)
+}
+function getTitle() {
+  if (!props.slider.translate) return props.slider.title
+  return lang(props.slider.title)
+}
+function change(e) {
+  if (!mounted) return
+
+  let value = e.target.value
+  e.target.blur()
+
+  if (value == props.slider.current) return
+  menuStore.setSliderCurrent({ index: props.index, value: parseInt(value) })
+}
+function leftKey() {
+  if (props.index == 0) return '←'
+  if (props.index == 1) {
+    if (menuStore.isQwerty)
+      return "q"
+    else
+      return "a"
   }
-  function leftKey() {
-    if (props.index == 0) return '←'
-    if (props.index == 1) {
-      if (menuStore.isQwerty)
-        return "q"
-      else
-        return "a"
-    }
-    if (props.index == 2) return "4"
-  }
-  function rightKey() {
-    if (props.index == 0) return '→'
-    if (props.index == 1) return "E"
-    if (props.index == 2) return "6"
-  }
-  onMounted(() => {
-    mounted = true
-  })
-  onBeforeUnmount(() => {
-    mounted = false
-  })
+  if (props.index == 2) return "4"
+}
+function rightKey() {
+  if (props.index == 0) return '→'
+  if (props.index == 1) return "E"
+  if (props.index == 2) return "6"
+}
+onMounted(() => {
+  mounted = true
+})
+onBeforeUnmount(() => {
+  mounted = false
+})
 </script>
