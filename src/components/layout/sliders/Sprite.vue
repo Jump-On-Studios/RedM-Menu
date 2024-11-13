97bf8fdffa085389c95ea0652b5abebc14d1e020
KadDarem <cressent.h@orange.fr>
2024-11-13 18:21:57 +0100

    Update slider sprite CSS

--- src/components/layout/sliders/Sprite.vue
+++ src/components/layout/sliders/Sprite.vue
@@ -1,21 +1,16 @@
 <template>
-  <template v-if="props.slider.values.length > 1">
-    <div :data-slider-index="props.index" class="slider">
-      <h2 v-if="props.slider.title">{{ title() }}</h2>
+  <template v-if="slider.values.length > 1">
+    <div :data-slider-index="index" class="slider">
+      <h2 v-if="slider.title">{{ title() }}</h2>
       <div class="arrows">
-        <div class="arrow left clicker" @click="menuStore.sliderLeft(props.index)"><img src="/assets/images/menu/selection_arrow_left.png"></div>
+        <div class="arrow left clicker" @click="menuStore.sliderLeft(index)"><img src="/assets/images/menu/selection_arrow_left.png"></div>
         <div class="text hapna">{{ numItem() }}</div>
-        <div class="arrow right clicker" @click="menuStore.sliderRight(props.index)"><img src="/assets/images/menu/selection_arrow_right.png"></div>
+        <div class="arrow right clicker" @click="menuStore.sliderRight(index)"><img src="/assets/images/menu/selection_arrow_right.png"></div>
       </div>
-      <div class="sprites" id="scroller">
-        <div v-for="(value,vIndex) in props.slider.values"
-          :key="vIndex+1"
-          :class="['sprite clicker',{'current' : (vIndex+1) == props.slider.current}]"
-          :id="'sprite-'+(vIndex+1)"
-          @click="click(vIndex+1)"
-        >
+      <div :class="['sprites', { 'center': slider.values.length <= 8 }]" id="scroller">
+        <div v-for="(value, vIndex) in slider.values" :key="vIndex + 1" :class="['sprite clicker', { 'current': (vIndex + 1) == slider.current }]" :id="'sprite-' + (vIndex + 1)" @click="click(vIndex + 1)">
           <img :src="`./assets/images/${value.sprite}.png`" />
-          <div class="tick" v-if="props.slider.displayTick && props.slider.tickIndex == vIndex">
+          <div class="tick" v-if="slider.displayTick && slider.tickIndex == vIndex">
             <img src="/assets/images/menu/tick.png">
           </div>
         </div>
@@ -31,25 +26,25 @@ const lang = useLangStore().lang
 import { useMenuStore } from '../../../stores/menus';
 const menuStore = useMenuStore()
 
-const props = defineProps(['slider','index'])
+const { slider, index } = defineProps(['slider', 'index'])
 
 const API = inject('API')
 
 function title() {
-  if (props.slider.translate)
-    return lang(props.slider.title)
-  return props.slider.title
+  if (slider.translate)
+    return lang(slider.title)
+  return slider.title
 }
 
 function numItem() {
-  return API.sprintf(lang('of'),props.slider.current, props.slider.values.length)
+  return API.sprintf(lang('of'), slider.current, slider.values.length)
 }
 function click(vIndex) {
-  if (vIndex == props.slider.current) return
-  menuStore.setSliderCurrent({index: props.index,value:parseInt(vIndex)})
+  if (vIndex == slider.current) return
+  menuStore.setSliderCurrent({ index: index, value: parseInt(vIndex) })
 }
 
-function scrollToElementHOrizontal(scroller,element) {
+function scrollToElementHOrizontal(scroller, element) {
   if (scroller == undefined) return
   if (element == undefined) return
   const scrollerRect = scroller.getBoundingClientRect();
@@ -58,10 +53,10 @@ function scrollToElementHOrizontal(scroller,element) {
   const elementLeft = elementRect.left - scrollerRect.left + scrollerScrollLeft;
   const elementRight = elementRect.right - scrollerRect.left + scrollerScrollLeft;
   if (elementRect.left < scrollerRect.left) {
-      scroller.scrollTo({
-        left: elementLeft,
-        behavior: 'smooth'
-      });
+    scroller.scrollTo({
+      left: elementLeft,
+      behavior: 'smooth'
+    });
   }
   else if (elementRect.right > scrollerRect.right) {
     scroller.scrollTo({
@@ -74,8 +69,8 @@ function scrollToElementHOrizontal(scroller,element) {
 function updateScroll() {
   nextTick(() => {
     const scroller = document.querySelector('#scroller');
-    const currentItem = document.querySelector('#scroller #sprite-'+props.slider.current)
-    scrollToElementHOrizontal(scroller,currentItem)
+    const currentItem = document.querySelector('#scroller #sprite-' + slider.current)
+    scrollToElementHOrizontal(scroller, currentItem)
   })
 }
 menuStore.$subscribe(() => {
